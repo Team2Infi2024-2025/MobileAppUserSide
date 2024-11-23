38cb0c26ae1db6f2131d379f664b53c681b208e9
owenstuckman <ostuckman@outlook.com>
2024-11-23 15:13:01 -0500

    fixed main so it now properly runs

--- student_health_tracker/lib/Startup/main.dart
+++ student_health_tracker/lib/Startup/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:student_health_tracker/Startup/home_page.dart';
 import 'package:student_health_tracker/Startup/themes.dart';
 
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -7,17 +8,10 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
   await Supabase.initialize(
-    url: 'https://tsewlrukrykkycootlsb.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRzZXdscnVrcnlra3ljb290bHNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkzNjcwNTUsImV4cCI6MjA0NDk0MzA1NX0.DNwdRirUSViojOqOOLHaJ_lW9u2jd1siOxqdmQWz6PU',
+    url: 'https://cwuqeskcuzbzxxdkkgno.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3dXFlc2tjdXpienh4ZGtrZ25vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3NjAyNjEsImV4cCI6MjA0NjMzNjI2MX0.ll5U0251lxqwQU8691ZHXPFnBO8otEQljVYtjvhGCFs',
   );
-  runApp(
-    MultiProvider(
-      providers: [
-
-      ],
-      child: MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
 // Get a reference your Supabase client
@@ -31,12 +25,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'F',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flu'),
     );
   }
 }
@@ -58,9 +52,11 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Bonvoyage',
+        title: 'Student Health Tracker',
         theme: Themes.themeData['Lavender'],
+        builder: (context, snapshot) {
+          return HomePage();
+        }
     );
   }
 }
-
