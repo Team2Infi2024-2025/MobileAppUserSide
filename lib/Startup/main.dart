978c7d66e517bc5a41a0a161bf16df8b5188d8ef
owenstuckman <ostuckman@outlook.com>
2024-12-02 16:35:47 -0500

    Simplify Main
    init supabase inside of future builder
    
    remove redundant home page

--- lib/Startup/main.dart
+++ lib/Startup/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:localstorage/localstorage.dart';
+import 'package:student_health_tracker/Helpers/database.dart';
 
 // in project pages
 import 'package:student_health_tracker/Startup/home_page.dart';
@@ -10,18 +11,15 @@ import 'package:student_health_tracker/Startup/themes.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../Helpers/stream_signal.dart';
 
-
 /*
 Dart entrypoint for app.
 
-inits app and home page.
+Initializes app, and selects page to run.
+
+In addition, initializes supabase instance
  */
 
 Future<void> main() async {
-  await Supabase.initialize(
-    url: 'https://cwuqeskcuzbzxxdkkgno.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3dXFlc2tjdXpienh4ZGtrZ25vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA3NjAyNjEsImV4cCI6MjA0NjMzNjI2MX0.ll5U0251lxqwQU8691ZHXPFnBO8otEQljVYtjvhGCFs',
-  );
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
@@ -41,39 +39,19 @@ final StreamController<StreamSignal> mainStream = StreamController<StreamSignal>
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of the application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'F',
+      title: 'Student Health Tracker',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flu'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final int _selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Student Health Tracker',
-        theme: Themes.themeData['Lavender'],
-        builder: (context, snapshot) {
-          return HomePage();
-        }
+      home: FutureBuilder(
+          future: DataBase.init(),
+          builder: (context, snapshot){
+            return HomePage();})
     );
   }
 }
