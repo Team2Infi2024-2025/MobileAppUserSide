55747d567e218e10e8f17dd677b936fbd87d94fa
owenstuckman <ostuckman@outlook.com>
2025-01-31 21:45:33 -0500

    edge functions + auth revamp

--- lib/global_content/dynamic_content/auth.dart
+++ lib/global_content/dynamic_content/auth.dart
@@ -1,22 +1,101 @@
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'dart:convert';
 import 'package:crypto/crypto.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gotrue/src/types/user_attributes.dart';
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:student_health_tracker/global_content/static_content/global_widgets.dart';
+import 'package:student_health_tracker/main.dart';
+import 'package:student_health_tracker/startup/splash_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+import '../../Startup/home_page.dart';
 import 'database.dart';
 
 /*
-Similar to the database file, except this is used specifically for auth functions.
+Handles all auth functionalities (supabase auth)
 
-Not many functions here yet, but the idea is as oath expands and other sign ins get included, database will get messy.
+Frequently referenced in account page and login/sign-in process
  */
 
-// class to call helpers from
-class Auth {
+class AuthService {
+  // guest sign in
+  Future<void> signUpAnon(BuildContext context) async {
+    try {
+      await supabase.auth.signInAnonymously();
+      await DataBase.init();
+      if (context.mounted) {
+        Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => HomePage()),
+          (route) => false,
+        );
+      }
+    } on AuthException catch (error) {
+      if (context.mounted) {
+        context.showSnackBar(error.message);
+      }
+    }
+  }
 
+  // log out of current session
+  static void logOutAccount() async {
+    await supabase.auth.signOut();
+  }
+
+  // remove account row from profiles
+  static void resetAccount() async {
+    await supabase
+        .from('profiles')
+        .delete()
+        .eq('id', supabase.auth.currentUser!.id);
+  }
+
+  // deletes current user's account (based on the current session/user) and resets them to home
+  // will work with both anonymous sessions and actual accounts to clear all of their data
+  static Future<void> deleteAccount() async {
+    try {
+      if (supabase.auth.currentUser == null) {
+        throw Error();
+      }
+      // call edge function
+      await supabase.functions.invoke('delete-user');
+    } catch (error) {
+      print('Error when deleting a user.');
+    } finally {
+      // sign out, push back to home page
+      await supabase.auth.signOut();
+      GlobalWidgets.swipePage(SplashPage());
+    }
+  }
+
+  // send email to reset password
+  // see : https://supabase.com/docs/reference/javascript/auth-resetpasswordforemail
+  static void sendPasswordResetEmail(String email) async {
+    await supabase.auth.resetPasswordForEmail(email);
+  }
+
+  // update a user's password
+  static void updatePassword(newPassword) async {
+    await supabase.auth.updateUser(UserAttributes(
+        email: supabase.auth.currentUser?.email, password: newPassword));
+  }
+
+  static void updateEmail(newEmail) async {
+    await supabase.auth.updateUser(UserAttributes(email: newEmail));
+  }
+
+  // verifies email address
+  static bool checkEmail() {
+    final String? response = supabase.auth.currentUser?.email;
+    return response != null;
+  }
+
+  // update account information
+  static Future<void> updateAccount(int pkey, Map map) async {
+    await supabase.auth.updateUser(map as UserAttributes);
+  }
 
   /// Performs Apple sign in on iOS or macOS
-  // sample for now - non functional as requires supabase setup
   static Future<AuthResponse> signInWithApple() async {
     final rawNonce = supabase.auth.generateRawNonce();
     final hashedNonce = sha256.convert(utf8.encode(rawNonce)).toString();
@@ -41,5 +120,4 @@ class Auth {
       nonce: rawNonce,
     );
   }
-
 }
---
+++ supabase/functions/delete-user/index.ts
@@ -0,0 +1,42 @@
+import { serve } from 'https://deno.land/std@0.182.0/http/server.ts'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.14.0'
+import { corsHeaders } from '../_shared/cors.ts'
+import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+
+console.log(`Function "user-self-deletion" up and running!`)
+
+serve(async (req: Request) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+  try {
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabaseClient = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get('SUPABASE_URL') ?? '',
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+    // Now we can get the session or user object
+    const {
+      data: { user },
+    } = await supabaseClient.auth.getUser()
+    // And we can run queries in the context of our authenticated user
+    const { data: profiles, error: userError } = await supabaseClient.from('profiles').select('id, avatar_url')
+    if (userError) throw userError
+    const user_id = profiles[0].id
+    const supabaseAdmin = createClient(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_SERVICE_ROLE_KEY') ?? ''
+    )
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+})
