287b737cb173d4b3067e316b832584f63711b89f
manp242 <manp@vt.edu>
2025-03-06 03:10:05 -0500

    first commit- told u owen
    started adding stuff to the map page

--- lib/map_page/map_page.dart
+++ lib/map_page/map_page.dart
@@ -1,6 +1,40 @@
+import 'package:flutter/material.dart';
 
-/*
+class MapPage extends StatelessWidget {
+  const MapPage({super.key});
 
-Page where cases are displayed on a map.
-
- */
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: ElevatedButton(
+            onPressed: () {
+              print('Top button clicked!');
+            },
+            style: ElevatedButton.styleFrom(
+              minimumSize: const Size.fromHeight(50), // Make button full width
+            ),
+            child: const Text('Location: Blacksburg, VA'),
+          ),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Text('Read Terms and Conditions'),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print('Click!');
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(50), // Make button full width
+              ), 
+              child: const Text('Location: Blacksburg, VA'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
