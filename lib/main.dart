6b3c33ebb2d6d95d0994ff515c68011b29e5c212
owenstuckman <ostuckman@outlook.com>
2025-04-30 14:23:51 -0400

    IDPRO showcase updates

--- lib/main.dart
+++ lib/main.dart
@@ -24,67 +24,66 @@ also is the dart entrypoint (for android studio setup).
  */
 
 // Main Stream Controller for the application
-final StreamController<StreamSignal> mainStream = StreamController<StreamSignal>();
+final StreamController<StreamSignal> mainStream =
+    StreamController<StreamSignal>();
 
 Future<void> main() async {
+  // set orientation to up
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
-	// set orientation to up
-	SystemChrome.setPreferredOrientations([
-		DeviceOrientation.portraitUp,
-	]);
+  // wait for local storage to init
+  await initLocalStorage();
 
-	// wait for local storage to init
-	await initLocalStorage();
+  // init supabase (database for the project)
+  await DataBase.init();
 
-	// init supabase (database for the project)
-	await DataBase.init();
-
-	// ensure stream signal is started
-	if(!mainStream.hasListener){
-		runApp(const MyApp());
-	}
-
+  // ensure stream signal is started
+  if (!mainStream.hasListener) {
+    runApp(const MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
-	const MyApp({super.key});
+  const MyApp({super.key});
 
-	// This widget is the root of the application
-	// inits the app, starts everything
-	@override
-	Widget build(BuildContext context) {
-		// streambuilder helps with dynamic content
-		return StreamBuilder(
-				stream: mainStream.stream,
-				initialData: StreamSignal(streamController: mainStream, newData: {
-					'theme': localStorage.getItem('theme') ?? CustomThemes.mainTheme,
-				}),
-				builder: (context, snapshot) {
-					// return app
-					return MaterialApp(
-						title: 'Student Health Tracker',
-						theme: CustomThemes.themeData[snapshot.data?.data['theme']] ??
-								CustomThemes.mainTheme,
-						home: FutureBuilder(
-								future: DataBase.init(),
-								builder: (context, snapshot){
-									// starting page of the application
-									// set to splashpage
-									return StudentHealthChatApp();})
-					);
-				});
-	}
+  // This widget is the root of the application
+  // inits the app, starts everything
+  @override
+  Widget build(BuildContext context) {
+    // streambuilder helps with dynamic content
+    return StreamBuilder(
+        stream: mainStream.stream,
+        initialData: StreamSignal(streamController: mainStream, newData: {
+          'theme': localStorage.getItem('theme') ?? CustomThemes.mainTheme,
+        }),
+        builder: (context, snapshot) {
+          // return app
+          return MaterialApp(
+              title: 'Student Health Tracker',
+              theme: CustomThemes.themeData[snapshot.data?.data['theme']] ??
+                  CustomThemes.mainTheme,
+              home: FutureBuilder(
+                  future: DataBase.init(),
+                  builder: (context, snapshot) {
+                    // starting page of the application
+                    // set to splashpage
+                    return SplashPage();
+                  }));
+        });
+  }
 }
 
 extension ContextExtension on BuildContext {
-	void showSnackBar(String message, {bool isError = false}) {
-		ScaffoldMessenger.of(this).showSnackBar(
-			SnackBar(
-				content: Text(message, overflow: TextOverflow.fade),
-				backgroundColor: isError
-						? Theme.of(this).colorScheme.error
-						: Theme.of(this).snackBarTheme.backgroundColor,
-			),
-		);
-	}
+  void showSnackBar(String message, {bool isError = false}) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+        content: Text(message, overflow: TextOverflow.fade),
+        backgroundColor: isError
+            ? Theme.of(this).colorScheme.error
+            : Theme.of(this).snackBarTheme.backgroundColor,
+      ),
+    );
+  }
 }
